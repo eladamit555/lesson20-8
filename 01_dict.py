8cf2261437144d6b83c17dfa6dc2595f5aacc4e3
eladamit555 <eladamit555@gmail.com>
2026-08-20 19:25:15 +0300

    .

---
+++ 01_dict.py
@@ -0,0 +1,14 @@
+
+person = {'first name': 'Elad', 'last name': 'Amit', 'age': 23,
+          'address': {'city': 'sarona', 'street': 'hagefen', 'house number': 38},
+          'kids': None, 'hobbies': ['gym', 'football'], 'cars': 'Toyota', 'Gender': 'F',
+          'vegan': False}
+print(person)
+print('first name:', person['first name'])
+print('last name:', person['last name'])
+print('number of kids:', person['kids'])
+print('hobbies:', person['hobbies'][0])
+print('address', person['address']['city'])
+
+
+
---
+++ 02_dict.py
@@ -0,0 +1,23 @@
+import random
+items_options = {'sword', 'shield', 'staff', 'mace', 'ring', 'boots'}
+item_icons_set = {'⚔️', '🛡️', '🪄', '🔨', '💍', '🥾'}
+attributes = {
+    'name': None,
+    'strength': 15, 'dexterity': 12, 'constitution': 14, 'intelligence': 10, 'wisdom': 13, 'charisma': 8,
+    'inventory': []
+}
+while True:
+    name = input('Enter your name: ')
+    attributes.update({'name': name})
+    attributes['strength'] = random.randint(3, 18)
+    attributes['dexterity'] = random.randint(3, 18)
+    attributes['constitution'] = random.randint(3, 18)
+    attributes['intelligence'] = random.randint(3, 18)
+    attributes['wisdom'] = random.randint(3, 18)
+    attributes['charisma'] = random.randint(3, 18)
+    attributes['inventory'] = random.sample(list(items_options), 2)
+    print(attributes)
+    answer = input('want to change? (yes/no)')
+    if answer != 'yes':
+        break
+
